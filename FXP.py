9ed254e1a77644c20cd57508f4c3076f9a9144a5
Jackson Wen <Jacksonwen026@gmail.com>
2019-06-24 15:41:00 -0500

    Enter

---
+++ FXP.py
@@ -0,0 +1,18 @@
+rqe = int(input('Do you need help? 1.yes 2.no\n'))
+Balance = 100
+if rqe == 1:
+    wyn = int(input('1.Deposit/withdraw 2.Exchanged 3.Info\n'))
+    if wyn == 1:
+        num = int(input('How Much? Negative amount for withdraw\n'))
+        Balance +=num
+        print('Now balance is '+str(Balance))
+    elif wyn ==2:
+        exn=float(int(input('Exchange rate now is 1 Gold = 51.3RMB, enter the amount of Gold you want\n')))
+        tt= exn*51.3
+        print('System will exchange '+str(exn)+' Gold for '+str(tt)+" from your account" )
+        Balance-=tt
+        print('Now your balance is ' +str(Balance))
+    else:
+        print('There is nothing special')
+else:
+    print('Have a good day and btw your balance is' +str(Balance))
