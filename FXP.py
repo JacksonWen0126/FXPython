93344c00877e8b3ae8266c924d4be8ccb5e271b3
Jackson Wen <Jacksonwen026@gmail.com>
2019-06-24 16:34:14 -0500

    Add read file

--- FXP.py
+++ FXP.py
@@ -1,5 +1,19 @@
+
+
+
+
+
+def load1():
+    f=open('SecretFile.txt','r')
+    f1 = f.read().split(',')
+    Balance=f1[1]
+    #print(Balance)
+    return Balance
+    
+Balance=load1()
 rqe = int(input('Do you need help? 1.yes 2.no\n'))
-Balance = 100
+
+
 if rqe == 1:
     wyn = int(input('1.Deposit/withdraw 2.Exchanged 3.Info\n'))
     if wyn == 1:
@@ -13,6 +27,8 @@ if rqe == 1:
         Balance-=tt
         print('Now your balance is ' +str(Balance))
     else:
+        
         print('There is nothing special and your balance '+str(Balance))
+        
 else:
     print('Have a good day and btw your balance is ' +str(Balance))
